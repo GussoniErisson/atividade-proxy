c0b32b075b4474a2869d3ab16b419caea52ea568
GussoniErisson <168599540+GussoniErisson@users.noreply.github.com>
2024-09-13 16:59:30 -0300

    Add files via upload

---
+++ src/main/java/com/proxy/pattern/Main.java
@@ -0,0 +1,17 @@
+package com.proxy.pattern;
+
+import java.math.BigDecimal;
+
+import model.Orcamento;
+import model.OrcamentoProxy;
+
+public class Main {
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal ("300"), new BigDecimal(10));
+        System.out.println("Valor do orcament0 =" + orcamento.getValorOrcamento());
+        System.out.println("Tempo para ver o desconto ="  +orcamento.getDescontoOrcamento());
+
+        OrcamentoProxy proxy = new OrcamentoProxy(orcamento);
+        System.out.println("Valor com proxy =" +proxy.getDescontoOrcamento());
+    }
+}
